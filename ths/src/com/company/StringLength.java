bf677db7f6770b0fed71ec0fb3474d58805692e7
ths <tianhaisu@yundait.com>
2020-07-02 11:20:54 +0800

    字符串格式

---
+++ ths/src/com/company/StringLength.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * 字符串长度
+ *
+ * @author zhanglei
+ * @date 2020/3/13 11:44
+ */
+public class StringLength {
+    public static void main(String[] args) {
+        String name = "张雷";
+        String pinyin = "zhanglei";
+        System.out.println(name.length());
+        System.out.println(pinyin.length());
+    }
+}
